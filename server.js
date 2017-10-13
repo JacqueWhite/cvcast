9528e608b5bc8f987850d820af678634c700149c
afletch24 <anfletcher24@gmail.com>
2017-10-13 12:34:14 -0500

    initial route setup

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-// const routes = require("./routes");
+const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use(express.static("client/build"));
 
 // Add routes, both API and view
-// app.use(routes);
+app.use(routes);
 
 // Set up promises with mongoose
 mongoose.Promise = global.Promise;
@@ -22,12 +22,12 @@ mongoose.Promise = global.Promise;
 //===========================================================================================
 //Remember to change below.
 // Connect to the Mongo DB
-// mongoose.connect(
-//   process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist",
-//   {
-//     useMongoClient: true
-//   }
-// );
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/showAndTell",
+  {
+    useMongoClient: true
+  }
+);
 //===========================================================================================
 
 // Start the API server
