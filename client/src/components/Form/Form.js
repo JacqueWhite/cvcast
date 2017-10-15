6aebc30bd9b9656465336a290d772eb4e8960b1a
lisanewhouse <lisanewhouse@icloud.com>
2017-10-15 18:06:08 -0500

    title

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,10 +1,16 @@
 import React from "react";
-import {Col, Card, CardTitle} from 'react-materialize';
+import {Col, Card, CardTitle, Button, Modal, Icon} from 'react-materialize';
 import "./Form.css";
 
 
 const Form = props => (
-	
+
+	<Modal
+	header='Modal Header'
+	trigger={<Button waves='light'>OR ME!<Icon right>insert_chart</Icon></Button>}>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+		incididunt ut labore et dolore magna aliqua.</p>
+</Modal>	
 
 );
 
