324988629ffa669d152d97fa517f13d17c52e7b4
lisanewhouse <lisanewhouse@icloud.com>
2017-10-15 17:59:02 -0500

    add Form

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Display from "./pages/display";
-
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+// import Form from "./components/Form";
 
 
 const App = () =>
   <Router>
     <div>
-      <Switch>
-        <Route exact path="/" component={Display} />
-      </Switch>
+    	<Wrapper>
+	      <Switch>
+	        <Route exact path="/" component={Display} />
+	      </Switch>
+	    </Wrapper>
     </div>
   </Router>;
 
---
+++ client/src/components/Form/Form.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {Col, Card, CardTitle} from 'react-materialize';
+import "./Form.css";
+
+
+const Form = props => (
+	
+
+);
+
+export default Form;
