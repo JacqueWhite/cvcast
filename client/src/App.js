7012ec6299fc8684abbcb6ae1aec3b7eb9ddcc1f
lisanewhouse <lisanewhouse@icloud.com>
2017-10-16 18:35:48 -0500

    update

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,13 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Display from "./pages/display";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
-
+import Form from "./components/Form";
 
 const App = () =>
   <Router>
     <div>
     	<Title />
+      <Form />
     	<Wrapper>
 	      <Switch>
 	        <Route exact path="/" component={Display} />
