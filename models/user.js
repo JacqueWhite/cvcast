1967f289768b73157109f29bcdfaaec155cf955e
isai-solis <solis_isai@yahoo.com>
2017-10-16 15:18:11 -0500

    Added src folder with example functionality for project cards.

--- models/user.js
+++ models/user.js
@@ -1,1 +1,38 @@
 //parent schema
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  synopsis: String,
+  date: { type: Date, default: Date.now }
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema ({
+    firstName: {
+        type: String,
+        require: true
+    },
+    lastName: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true
+    },
+    projectList: {
+        any:[Schema.Types.Mixed]
+    }
+});
+const User = mongoose.model("User", userSchema);
+module.exports = User;
