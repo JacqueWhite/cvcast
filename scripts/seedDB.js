93e3241738a6ccf3e9e95e7ecc25c75440638e9b
afletch24 <anfletcher24@gmail.com>
2017-10-17 15:30:12 -0500

    fixed errors

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -10,41 +10,6 @@ mongoose.connect(
 );
 
 const userSeed = [
-<<<<<<< HEAD
-  {
-<<<<<<< HEAD
-      firstName: "Audrey",
-      lastName: "Fletcher",
-      email: "anfletcher24@gmail.com"
-  },
-    {
-      firstName: "Jacque",
-      lastName: "White",
-      email: "Jacque@gmail.com"
-  },
-    {
-      firstName: "Jayme",
-      lastName: "Howard",
-      email: "Jayme@gmail.com"
-  },
-    {
-      firstName: "Lisa",
-      lastName: "NewHouse",
-      email: "Lisa@gmail.com"
-  },
-    {
-      firstName: "Isai",
-      lastName: "Solis",
-      email: "Isai@gmail.com"
-  },
-    {
-      firstName: "John",
-      lastName: "Torrence",
-      email: "John@gmail.com"
-=======
-    id: 1,
-    project: "LocALL",
-=======
     {
     _id: 1,
     firstName: "Jacqueline",
@@ -69,7 +34,6 @@ const projectSeed = [
   {
     _userId: 1,
     projectName: "LocALL",
->>>>>>> jacque
     image: "https://s3.us-east-2.amazonaws.com/jacqueportfolio/locall.png",
     description: "a place to get recommendations on where to eat, drink, and play in Austin, TX",
     technologiesKeywords: "CSS, javaScript, jQuery, Google Firebase, Bootstrap",
@@ -78,13 +42,8 @@ const projectSeed = [
     github: "https://github.com/JacqueWhite/LocAll"  
   },
   {
-<<<<<<< HEAD
-    id: 2,
-    project: "ItsAGO",
-=======
     _userId: 1,
     projectName: "ItsAGO",
->>>>>>> jacque
     image: "https://s3.us-east-2.amazonaws.com/jacqueportfolio/itsago.png",
     description: "Simple event and RSVP manager",
     technologiesKeywords: "CSS, javaScript, Node.js, Materialize, jQuery, MySQL, Sequelize",
@@ -93,13 +52,8 @@ const projectSeed = [
     github: "https://github.com/JacqueWhite/Its-a-GO"  
   },
   {
-<<<<<<< HEAD
-    id: 3,
-    project: "AccountabiliBuddy",
-=======
     _userId: 2,
     projectName: "AccountabiliBuddy",
->>>>>>> jacque
     image: "https://s3.us-east-2.amazonaws.com/jacqueportfolio/accountabilibuddy.png",
     description: "A gym-buddy match app that pairs up friends based on schedule and fitness activity preferences.",
     technologiesKeywords: "CSS, javaScript, Materialize, Node.js, jQuery, MySQL, Sequelize",
@@ -108,13 +62,8 @@ const projectSeed = [
     github: "https://github.com/JacqueWhite/AccountabiliBuddy"  
   },
   {
-<<<<<<< HEAD
-    id: 4,
-    project: "devScraped",
-=======
     _userId: 2,
     projectName: "devScraped",
->>>>>>> jacque
     image: "https://s3.us-east-2.amazonaws.com/jacqueportfolio/devscrape.png",
     description: "A tool for developers to scrape their favorite resource pages and take notes on them. ",
     technologiesKeywords: "CSS, javaScript, Materialize, jQuery, MySQL, Sequelize",
@@ -123,20 +72,15 @@ const projectSeed = [
     github: "https://github.com/JacqueWhite/devscraped"  
   },
   {
-<<<<<<< HEAD
-    id: 5,
-    project: "oneReq",
-=======
     _userId: 3,
     projectName: "oneReq",
->>>>>>> jacque
     image: "https://s3.us-east-2.amazonaws.com/jacqueportfolio/accountabilibuddy.png",
     description: "Recruiting platform for Software Developer recruiters.",
     technologiesKeywords: "CSS, javaScript, Bootstrap, React, MongoDB, mongoose",
     team: "Jacqueline White",
     link: "https://onereq.herokuapp.com/",
     github: "ttps://github.com/JacqueWhite/onereq"  
->>>>>>> 89092607cda4aeef5472ac2aff3301a2eb9ce119
+
   }
 ];
 
@@ -151,8 +95,6 @@ db.User
   .catch(err => {
     console.error(err);
     process.exit(1);
-<<<<<<< HEAD
-=======
   });
 
 db.Project
@@ -165,5 +107,4 @@ db.Project
   .catch(err => {
     console.error(err);
     process.exit(1);
->>>>>>> jacque
   });
