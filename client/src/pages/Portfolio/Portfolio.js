88fc94763dfe4a67ac390e4436150931bb91ba15
isai-solis <solis_isai@yahoo.com>
2017-10-18 15:22:19 -0500

    Added getUser function to Portfolio.js, modified seedsDB., deleted mainController

--- client/src/pages/Portfolio/Portfolio.js
+++ client/src/pages/Portfolio/Portfolio.js
@@ -1,21 +1,64 @@
 import React, { Component } from "react";
 import {Row} from 'react-materialize';
 import PortfolioCard from "../../components/PortfolioCard";
-import Title from "../../components/Title";
-import user from "../../user.json";
+// import Title from "../../components/Title";
+import API from "../../utils/api";
 import "../../App.css";
 
 class Portfolio extends Component {
 
 // Setting this.state.friends to the portfoliocards json array
   state = {
-    user
+    user,
+    projects: []
   };
+
+  loadUser = () => {
+    API.getUser()
+      .then(res =>
+        this.setState({ user: res.data})
+      )
+      .catch(err => console.log(err));
+  };
+
+  loadProjects = () => {
+    API.getProjects()
+      .then(res =>
+        this.setState({ projects: res.data})
+      )
+      .catch(err => console.log(err));
+  };
+
+  deleteProject = id => {
+    API.deleteProject(id)
+      .then(res => this.loadProjects())
+      .catch(err => console.log(err));
+  };
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  // handleFormSubmit = event => {
+  //   event.preventDefault();
+  //   if (this.state.title && this.state.author) {
+  //     API.saveBook({
+  //       title: this.state.title,
+  //       author: this.state.author,
+  //       synopsis: this.state.synopsis
+  //     })
+  //       .then(res => this.loadProjects())
+  //       .catch(err => console.log(err));
+  //   }
+  // };
 
   render() {
     return (
     <div>
-        <Row>
+        {/* <Row>
           <Title
             name={this.state.user.name}
             linkedIn={this.state.user.linkedIn}
@@ -23,9 +66,9 @@ class Portfolio extends Component {
             email={this.state.user.email}
             bio={this.state.user.bio}
           />
-        </Row>
+        </Row> */}
         <Row>
-          {this.state.user.projects.map(portfoliocard => (
+          {this.state.projects.map(portfoliocard => (
             <PortfolioCard
               id={portfoliocard.id}
               key={portfoliocard.id}
