c1cbcea1383c41316b2ea8632945a7e593da4940
isai-solis <solis_isai@yahoo.com>
2017-10-19 18:10:07 -0500

    Removed node_modules.

--- models/user.js
+++ models/user.js
@@ -1,8 +1,12 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const Project = require('../models/project');
-
-const UserSchema = new Schema ({
+const filePluginLib = require('mongoose-file');
+const filePlugin = filePluginLib.filePlugin;
+const make_upload_to_model = filePluginLib.make_upload_to_model;
+const path = require("path");
+const uploads_base = path.join(__dirname, "../uploads");
+const uploads = path.join(uploads_base, "u");
+const userSchema = new Schema ({
     firstName: {
         type: String,
         require: true
@@ -18,7 +22,7 @@ const UserSchema = new Schema ({
         match: [/.+\@.+\..+/, "Please enter a valid e-mail address"]
     },
     headshot: {
-        type: String
+        img: { data: Buffer, contentType: String }
     },
     linkedIn: {
         type: String
@@ -31,15 +35,14 @@ const UserSchema = new Schema ({
     },
     projects: [{
         type: Schema.Types.ObjectId,
-        ref: "Project"
-    }],
-    createdBy: {
-        type: Schema.Types.ObjectId,
-        ref: "Project",
-        index: true
-    }
+        ref: "project"
+    }]
 });
-
-const User = mongoose.model("User", UserSchema);
-
+userSchema.plugin(filePlugin, {
+    name: "photo",
+    upload_to: make_upload_to_model(uploads, 'photos'),
+    relative_to: uploads_base
+});
+console.log("i'm in models/user.js");
+const User = mongoose.model("User", userSchema);
 module.exports = User;
