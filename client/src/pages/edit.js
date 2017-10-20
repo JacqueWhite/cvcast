c3a8046729316c51e5a98e80ee08f23c21806009
lisanewhouse <lisanewhouse@icloud.com>
2017-10-20 15:02:16 -0500

    api to Api

--- client/src/pages/edit.js
+++ client/src/pages/edit.js
@@ -4,7 +4,7 @@ import Nav from '../components/Nav';
 import PortfolioCardEdit from "../components/PortfolioCardEdit";
 import TitleCard from "../components/TitleCard";
 import ProjectForm from "../components/ProjectForm";
-import API from "../utils/api"
+import API from "../utils/Api"
 
 class Edit extends Component {
 
