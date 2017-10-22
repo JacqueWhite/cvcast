e320e585551f0481a0921a90c8158f9cc4070176
JacqueWhite <jacquecwhite@gmail.com>
2017-10-22 14:27:41 -0500

    added more to the porftoliocard

--- client/src/components/PortfolioCard/PortfolioCard.js
+++ client/src/components/PortfolioCard/PortfolioCard.js
@@ -11,7 +11,7 @@ const PortfolioCard = props => (
               </div>
               <div className="card-content">
                   <span className="card-title activator grey-text text-darken-4">{props.project}<i className="material-icons right">arrow_forward</i></span>
-                  <p><a href={props.link}>See it Live</a></p>
+                  <p><a href={props.link} target="_blank">See it Live</a></p>
                   <p>{props.description}</p>
               </div>
               <div className="card-action">
@@ -20,10 +20,15 @@ const PortfolioCard = props => (
                   ))}
               </div>
               <div className="card-reveal">
-                  <span className="card-title grey-text text-darken-4">{props.project}<i className="material-icons right">close</i></span>
-                  <p>Description:{props.description}</p>
-                  <p>See the code:{props.github}</p>
-                  <p>The team:{props.team}</p>
+                  <span className="card-title grey-text text-darken-4"><a href={props.link} target="_blank">{props.project}</a><i className="material-icons right">close</i></span>
+                  <p><span className="about-project">What is it: </span><br/>{props.description}</p>
+                  <p><span className="about-project">See the code on <a href={props.github} target="_blank"> GitHub</a></span></p>
+                  <p><span className="about-project">Team: </span>{props.team}</p>
+                  <hr/>
+                  <p><span className="about-project">Technologies & Keywords: </span></p>
+                  {props.technologiesKeywords.map(tag => (
+                  <div className="chip"><a href={`/${tag}`} target="_blank">{tag}</a></div>
+                  ))}
               </div>
           </div>
       </div>
