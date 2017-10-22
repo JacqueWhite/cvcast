a61f4ecede0da7d77b7a0b1f94452900d8c76a38
lisanewhouse <lisanewhouse@icloud.com>
2017-10-22 16:06:32 -0500

    .

--- client/src/components/PortfolioCard/PortfolioCard.js
+++ client/src/components/PortfolioCard/PortfolioCard.js
@@ -3,35 +3,33 @@ import "./PortfolioCard.css";
 
 const PortfolioCard = props => (
 <div>
-      <div className="col s12 m6">
-          <div className="card portfolio-card">>>>>>>> d2838edbf117707c23b12b00bad80f02f33f22a1
-              <div className="card-image waves-effect waves-block waves-light">
-                  <img className="activator" src={props.image} alt={props.project}/>
-              </div>
-              <div className="card-content">
-                  <span className="card-title activator grey-text text-darken-4">{props.project}<i className="material-icons right">arrow_forward</i></span>
-                  <p><a href={props.link} target="_blank">See it Live</a></p>
-              </div>
-              <div className="card-action">
-                  {props.technologiesKeywords.map(tag => (
-                  <div className="chip"><a href={`/${tag}`} target="_blank">{tag}</a></div>
-                  ))}
-              </div>
-              <div className="card-reveal">
-                  <span className="card-title grey-text text-darken-4"><a href={props.link} target="_blank">{props.project}</a><i className="material-icons right">close</i></span>
-                  <p><span className="about-project">What is it: </span><br/>{props.description}</p>
-                  <p><span className="about-project">See the code on <a href={props.github} target="_blank"> GitHub</a></span></p>
-                  <p><span className="about-project">Team: </span>{props.team}</p>
-                  <hr/>
-                  <p><span className="about-project">Technologies & Keywords: </span></p>
-                  {props.technologiesKeywords.map(tag => (
-                  <div className="chip"><a href={`/${tag}`} target="_blank">{tag}</a></div>
-                  ))}
-              </div>
-          </div>
-      </div>
-
+  <div className="col s12 m6">
+    <div className="card-image waves-effect waves-block waves-light">
+        <img className="activator" src={props.image} alt={props.project}/>
+    </div>
+    <div className="card-content">
+        <span className="card-title activator grey-text text-darken-4">{props.project}<i className="material-icons right">arrow_forward</i></span>
+        <p><a href={props.link} target="_blank">See it Live</a></p>
+    </div>
+    <div className="card-action">
+        {props.technologiesKeywords.map(tag => (
+        <div className="chip"><a href={`/${tag}`} target="_blank">{tag}</a></div>
+        ))}
+    </div>
+    <div className="card-reveal">
+        <span className="card-title grey-text text-darken-4"><a href={props.link} target="_blank">{props.project}</a><i className="material-icons right">close</i></span>
+        <p><span className="about-project">What is it: </span><br/>{props.description}</p>
+        <p><span className="about-project">See the code on <a href={props.github} target="_blank"> GitHub</a></span></p>
+        <p><span className="about-project">Team: </span>{props.team}</p>
+        <hr/>
+        <p><span className="about-project">Technologies & Keywords: </span></p>
+        {props.technologiesKeywords.map(tag => (
+        <div className="chip"><a href={`/${tag}`} target="_blank">{tag}</a></div>
+        ))}
+    </div>    
+  </div>
 </div>
 );
+
 export default PortfolioCard;
 
