03eb6dfec558d323c2797020b76ccd33dc696227
afletch24 <anfletcher24@gmail.com>
2017-10-22 15:38:59 -0500

    big fixes

--- client/src/components/ProjectForm/ProjectForm.js
+++ client/src/components/ProjectForm/ProjectForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import API from "../../utils/api";
+import API from "../../utils/API";
 import "./ProjectForm.css";
 import {Row, Input, Button} from 'react-materialize';
 
