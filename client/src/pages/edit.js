b9ce4ea30c67d92db9fd3a7c9f39d84c6e6016e9
lisanewhouse <lisanewhouse@icloud.com>
2017-10-22 17:02:17 -0500

    display name in Navbar

--- client/src/pages/edit.js
+++ client/src/pages/edit.js
@@ -87,7 +87,7 @@ class Edit extends Component {
     {console.log(this.state.currentProject)}
     return (
     <div>
-      <Nav/>
+      <Nav firstName={this.state.user.firstName} />
         <Row>
           <TitleCard
             firstName={this.state.user.firstName}
