3f32956df4046d49f4b5810f62593d9781bf133c
John <16932417+torrencj@users.noreply.github.com>
2017-10-23 20:19:08 -0500

    put the correct site url

--- client/src/components/Auth/auth0-variables.js
+++ client/src/components/Auth/auth0-variables.js
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
   domain: 'showandtellapp.auth0.com',
   clientId: 'CkQRdOqFw9REhOLGFJVGeUBEeM2wUNl8',
-  callbackUrl: 'https://showandtellapp.herokuapp.com/callback',
+  callbackUrl: 'https://showandtell.herokuapp.com/callback',
   apiUrl: 'https://showandtellapp.auth0.com/api/v2/'
 }
