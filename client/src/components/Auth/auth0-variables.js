9fafea4861e7933ef64415ee09fddfb2a0df623f
John <16932417+torrencj@users.noreply.github.com>
2017-10-23 19:52:17 -0500

    changed process env vars to support callback url on heroku

--- client/src/components/Auth/auth0-variables.js
+++ client/src/components/Auth/auth0-variables.js
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
   domain: 'showandtellapp.auth0.com',
   clientId: 'CkQRdOqFw9REhOLGFJVGeUBEeM2wUNl8',
-  callbackUrl: 'http://localhost:3000/callback',
+  callbackUrl: process.env.CALLBACKURL || 'http://localhost:3000/callback',
   apiUrl: 'https://showandtellapp.auth0.com/api/v2/'
 }
