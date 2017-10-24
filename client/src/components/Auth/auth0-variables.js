9fb7c5c59d739f645fa051f48100ce71785c9bef
John <16932417+torrencj@users.noreply.github.com>
2017-10-23 20:01:50 -0500

    changed callback url for heroku

--- client/src/components/Auth/auth0-variables.js
+++ client/src/components/Auth/auth0-variables.js
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
   domain: 'showandtellapp.auth0.com',
   clientId: 'CkQRdOqFw9REhOLGFJVGeUBEeM2wUNl8',
-  callbackUrl: process.env.CALLBACKURL || 'http://localhost:3000/callback',
+  callbackUrl: 'https://showandtellapp.herokuapp.com/callback',
   apiUrl: 'https://showandtellapp.auth0.com/api/v2/'
 }
