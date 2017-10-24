7cc5deffbfbec7d5d610e8b47016c18d0beb11da
John <16932417+torrencj@users.noreply.github.com>
2017-10-24 09:22:53 -0500

    setup production for heroku

--- client/src/components/Auth/auth0-variables.js
+++ client/src/components/Auth/auth0-variables.js
@@ -1,7 +1,7 @@
 export const AUTH_CONFIG = {
   domain: 'showandtellapp.auth0.com',
   clientId: 'CkQRdOqFw9REhOLGFJVGeUBEeM2wUNl8',
-  // callbackUrl: 'https://showandtell.herokuapp.com/callback',
-  callbackUrl: 'http://localhost:3000/callback',
+  callbackUrl: 'https://showandtell.herokuapp.com/callback',
+  // callbackUrl: 'http://localhost:3000/callback',
   apiUrl: 'https://showandtellapp.auth0.com/api/v2/'
 }
