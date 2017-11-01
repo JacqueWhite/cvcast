b15da84db7fa499fd080a0dab3e794a0665b7718
JacqueWhite <jacquecwhite@gmail.com>
2017-11-01 13:25:21 -0500

    heroku

--- client/src/components/Auth/auth0-variables.js
+++ client/src/components/Auth/auth0-variables.js
@@ -7,7 +7,7 @@
 
 export const AUTH_CONFIG = {
   domain: 'cvcast.auth0.com',
-  clientId: 'HawvbyK2KS0NlGbRnGRduKVH5I5v2ip3',
+  clientID: 'HawvbyK2KS0NlGbRnGRduKVH5I5v2ip3',
   callbackUrl: 'https://cvcast.herokuapp.com/callback',
   apiUrl: 'https://cvcast.auth0.com/api/v2/'
 }
