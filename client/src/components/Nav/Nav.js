c0f4d428aad5d529eceabeeb2354416286ea43c6
JacqueWhite <jacquecwhite@gmail.com>
2017-11-27 18:16:29 -0600

    fixing navbar

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,17 +1,27 @@
 import React from 'react';
-import {Navbar, Icon, NavItem} from 'react-materialize';
 import './Nav.css';
 
-
 const Nav = props => (
-
-	<Navbar brand='cvcast' fixed right>
-		<NavItem href='/edit'><Icon>edit</Icon></NavItem>
-		<NavItem href='/portfolio'><Icon>public</Icon></NavItem>
-		<NavItem href=''><Icon>link</Icon></NavItem>
-		<NavItem href='/main'><Icon>portrait</Icon></NavItem>
-		<NavItem>Hello {props.firstName}!</NavItem>
-	</Navbar>
+<div>
+  <nav>
+    <div className="nav-wrapper">
+      <a href="/" className="brand-logo">cvcast</a>
+      <a href="#" data-activates="nav-mobile" className="button-collapse"><i className="material-icons">menu</i></a>
+      <ul className="right hide-on-med-and-down">
+        <li><a href='/edit'><i class="material-icons">edit</i>edit</a></li>
+        <li><a href='/portfolio'><i class="material-icons">public</i>cv</a></li>
+        <li><a href='/main'><i class="material-icons">home</i>home</a></li>
+        <li>Hi {props.firstName}!</li>
+      </ul>
+      <ul className="side-nav" id="nav-mobile">
+        <li><a href='/edit'><i class="material-icons">edit</i>edit</a></li>
+        <li><a href='/portfolio'><i class="material-icons">public</i>cv</a></li>
+        <li><a href='/main'><i class="material-icons">home</i>home</a></li>
+        <li>Hi {props.firstName}!</li>
+      </ul>
+    </div>
+  </nav>
+</div>
 );
 
 export default Nav;
