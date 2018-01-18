fdee1abfce8dcfe43e7781777910ed7a660d7b30
JacqueWhite <jacquecwhite@gmail.com>
2018-01-18 17:02:37 -0600

    adding callback url

--- client/src/components/Auth/auth0-variables.js
+++ client/src/components/Auth/auth0-variables.js
@@ -6,13 +6,13 @@
 //   apiUrl: 'https://cvcast.auth0.com/api/v2/'
 // }
 
-export const AUTH_CONFIG = {
-  domain: 'cvcast.auth0.com',
-  clientID: 'HawvbyK2KS0NlGbRnGRduKVH5I5v2ip3',
-  clientID2: 'RWYBVchg08eb_KgUP5di78eeevbm_sNg',
-  callbackUrl:'https://cvcast.io/callback',
-  apiUrl: 'https://cvcast.auth0.com/api/v2/'
-}
+// export const AUTH_CONFIG = {
+//   domain: 'cvcast.auth0.com',
+//   clientID: 'HawvbyK2KS0NlGbRnGRduKVH5I5v2ip3',
+//   clientID2: 'RWYBVchg08eb_KgUP5di78eeevbm_sNg',
+//   callbackUrl:'https://cvcast.io/callback',
+//   apiUrl: 'https://cvcast.auth0.com/api/v2/'
+// }
 
 // export const AUTH_CONFIG = {
 //   domain: 'cvcast.auth0.com',
@@ -22,11 +22,11 @@ export const AUTH_CONFIG = {
 //   apiUrl: 'https://cvcast.auth0.com/api/v2/'
 // }
 
-// export const AUTH_CONFIG = {
-//   domain: 'cvcast.auth0.com',
-//   clientID: 'HawvbyK2KS0NlGbRnGRduKVH5I5v2ip3',
-//   clientID2: 'RWYBVchg08eb_KgUP5di78eeevbm_sNg',
-//   callbackUrl: 'https://cvcast.herokuapp.com/callback',
-//   apiUrl: 'https://cvcast.auth0.com/api/v2/'
-// }
+export const AUTH_CONFIG = {
+  domain: 'cvcast.auth0.com',
+  clientID: 'HawvbyK2KS0NlGbRnGRduKVH5I5v2ip3',
+  clientID2: 'RWYBVchg08eb_KgUP5di78eeevbm_sNg',
+  callbackUrl: 'https://cvcast.herokuapp.com/callback',
+  apiUrl: 'https://cvcast.auth0.com/api/v2/'
+}
 
